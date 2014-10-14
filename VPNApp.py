68da1f2cfa6ad8cdf5c56584bc8373624a63111a
Jorden Hetherington <jorden.hetherington@gmail.com>
2014-10-14 12:38:54 -0700

    fixing slight MAC issue

--- VPNApp.py
+++ VPNApp.py
@@ -156,8 +156,8 @@ class VPNApp(Frame):
         if to_send and self.connector:
             encrypted = aes.aes_encrypt(to_send, self.session_key)
             encoded = bytes(encrypted)
-            mac = mac.get_mac(to_send, MAC_KEY)
-            self.connector.send(encoded+mac)
+            mac_val = mac.get_mac(to_send, MAC_KEY)
+            self.connector.send(encoded+mac_val)
 
     def continue_callback(self):
         pass
@@ -203,11 +203,11 @@ def connect(host, port, shared_value, is_server):
     shared_val = shared_value.encode('utf-8')
     md5_key.update(shared_val)
     long_term_key = md5_key.digest()
-	
-	md5_key = hashlib.md5()
-	md5_key.update(long_term_key)
-	MAC_KEY = md5_key.digest()
-	
+    
+    md5_key = hashlib.md5()
+    md5_key.update(long_term_key)
+    MAC_KEY = md5_key.digest()
+    
     
     ctr.connect()
     
@@ -267,12 +267,12 @@ def connect(host, port, shared_value, is_server):
         rcv_client_public_transport = ctr.receive_wait()
         
         # Authenticate received data from client
-        expect_rcv_client_auth_msg = rcv_client_id + server_nonce
+        expect_rcv_client_auth_msg = list(rcv_client_id) + list(server_nonce)
         session_key = dh_auth.gen_session_key(rcv_client_public_transport, server_dh_data_tup[dh_auth.LOC_EXPONENT_IDX], long_term_key, expect_rcv_client_auth_msg)
 
-	if session_key == 0:
-		logging.getLogger().info('Failed to authenticate: session key invalid')
-	
+    if session_key == 0:
+        logging.getLogger().info('Failed to authenticate: session key invalid')
+
     # Enforce Perfect Forward Security by forgetting local exponent 
     client_dh_data_tup = (0,0)
     server_dh_data_tup = (0,0)
