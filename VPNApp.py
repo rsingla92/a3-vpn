521ddadda7597e7b41fa83644b7a768871b4f44a
Ricky Singla <rsingla92@gmail.com>
2014-10-14 19:12:23 -0700

    Fix log

--- VPNApp.py
+++ VPNApp.py
@@ -296,6 +296,7 @@ def connect(host, port, shared_value, is_server):
     if session_key == 0:
         logging.getLogger().info('Failed to authenticate: session key invalid')
 
+    logging.getLogger().info('Authenticated with session key: ' + session_key)
     # Enforce Perfect Forward Security by forgetting local exponent 
     client_dh_data_tup = (0,0)
     server_dh_data_tup = (0,0)
